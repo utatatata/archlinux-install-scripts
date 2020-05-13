80f87910e5ab604ad8558c565e87a87fbe4d81d6
utakuma <40954814+utatatata@users.noreply.github.com>
2020-05-14 04:43:34 +0900

    fix gui_i3.sh

--- gui_i3.sh
+++ gui_i3.sh
@@ -239,13 +239,7 @@ curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fi
 cat <<EOF >~/.config/fish/fishfile
 oh-my-fish/theme-bobthefish 
 EOF
-cat <<EOF >~/.config/fish/config.fish
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo \$XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-EOF
+fish -c fisher
 
 #################### FINISH ####################
 
