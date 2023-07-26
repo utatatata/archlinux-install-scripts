8c7a48a09e5e370a1abe7e6d574c8ebfb81e2ac8
utakuma <utatatata.kuma@gmail.com>
2023-07-27 04:07:03 +0900

    delete ansi.sh

--- gui-i3.sh
+++ gui-i3.sh
@@ -2,7 +2,29 @@
 
 set -eu
 
-source ./ansi.sh
+# ANSI escape code (https://en.wikipedia.org/wiki/ANSI_escape_code)
+prefix="\x1b["
+suffix="m"
+RESET="${prefix}${suffix}"
+RED="${prefix}31${suffix}"
+GREEN="${prefix}32${suffix}"
+CYAN="${prefix}36${suffix}"
+
+print_red() {
+  printf "${RED}${1}${RESET}"
+}
+print_green() {
+  printf "${GREEN}${1}${RESET}"
+}
+print_cyan() {
+  printf "${CYAN}${1}${RESET}"
+}
+
+error() {
+  # printf "${RED}error${RESET}: ${1}\n"
+  print_red "error";
+  printf ": ${1}\n"
+}
 
 
 
